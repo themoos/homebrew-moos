fa8a9d2a98e1cf9c4b8ac031d5680ea7bd0d0a76
Paul Newman <pnewman@robots.ox.ac.uk>
2012-12-11 18:11:23 +0000

    made it work on my machine

--- core-moos.rb
+++ core-moos.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class CoreMoos < Formula
   homepage ''
   url 'https://github.com/themoos/core-moos/archive/devel.zip'
@@ -10,20 +7,18 @@ class CoreMoos < Formula
   version '10.0.1'
   depends_on 'cmake' => :build
 
-  option 'use-async-comms', 'build with v10 asynchronous comms'
 
   def install
+      cmake_args = std_cmake_parameters.split
+      
+      cmake_args << "-DUSE_AYSNC_COMMS:BOOL=ON"
 
-      cmake_args = %W[
-      -DCMAKE_BUILD_TYPE=Release
-      -DENABLE_V10_COMPATIBILITY
-      ]
-
-      cmake_args << "-DUSE_AYSNC_COMMS" if build.include? 'use-async-comms'
-
-    system "cmake", ".", cmake_args
-    system "make"
-    system "make install"
+      cmake_args << "-DENABLE_V10_COMPATIBILITY:BOOL=ON"
+      
+      system "cmake", ".", *cmake_args
+      system "make"
+      system "make install"
+      
   end
 
 end
