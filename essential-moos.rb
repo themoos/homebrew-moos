e392705d9c15ab6cc726f006ba3f2e9e9ba60d51
Paul Newman <pnewman@robots.ox.ac.uk>
2012-12-12 11:58:43 +0000

    still working on it

--- essential-moos.rb
+++ essential-moos.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
-class CoreMoos < Formula
+class EssentialMoos < Formula
   homepage 'www.robots.ox.ac.uk/~pnewman/TheMOOS'
     #url 'https://github.com/themoos/essential-moos/archive/devel.zip'
-  head 'https://github.com/themoos/essential-moos.git' :branch => 'devel'
+  head 'https://github.com/themoos/essential-moos.git', :branch => 'devel'
     
   #sha1 'a077343435f6b0f2cfa22d23066dcb8c7571d852'
   version '10.0.1'
