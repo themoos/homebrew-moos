870d6678c07e40a807ffd2ee132e8c755c98cb1c
Paul Newman <pnewman@robots.ox.ac.uk>
2012-12-12 08:55:48 +0000

    added essential-moos.rb

--- essential-moos.rb
+++ essential-moos.rb
@@ -2,19 +2,15 @@ require 'formula'
 
 class CoreMoos < Formula
   homepage 'www.robots.ox.ac.uk/~pnewman/TheMOOS'
-  url 'https://github.com/themoos/core-moos/archive/devel.zip'
+  url 'https://github.com/themoos/essential-moos/archive/devel.zip'
   #sha1 'a077343435f6b0f2cfa22d23066dcb8c7571d852'
   version '10.0.1'
   depends_on 'cmake' => :build
-
+  depends_on 'core-moos'
+    
 
   def install
       cmake_args = std_cmake_parameters.split
-      
-      cmake_args << "-DUSE_AYSNC_COMMS:BOOL=ON"
-
-      cmake_args << "-DENABLE_V10_COMPATIBILITY:BOOL=ON"
-      
       system "cmake", ".", *cmake_args
       system "make"
       system "make install"
