dc90167c788476b0c767d82c0473ac7d6619104e
Paul Newman <pnewman@robots.ox.ac.uk>
2012-12-12 12:17:50 +0000

    added test and head

--- core-moos.rb
+++ core-moos.rb
@@ -1,24 +1,33 @@
 require 'formula'
 
 class CoreMoos < Formula
-  homepage 'www.robots.ox.ac.uk/~pnewman/TheMOOS'
-  url 'https://github.com/themoos/core-moos/archive/devel.zip'
-  #sha1 'a077343435f6b0f2cfa22d23066dcb8c7571d852'
-  version '10.0.1'
-  depends_on 'cmake' => :build
-
-
-  def install
-      cmake_args = std_cmake_parameters.split
-      
-      cmake_args << "-DUSE_AYSNC_COMMS:BOOL=ON"
-
-      cmake_args << "-DENABLE_V10_COMPATIBILITY:BOOL=ON"
-      
-      system "cmake", ".", *cmake_args
-      system "make"
-      system "make install"
-      
-  end
-
+    homepage 'www.robots.ox.ac.uk/~pnewman/TheMOOS'
+    url 'https://github.com/themoos/core-moos/archive/devel.zip'
+    
+    #sha1 'a077343435f6b0f2cfa22d23066dcb8c7571d852'
+    head 'https://github.com/themoos/core-moos.git', :branch => 'devel'
+    
+    version '10.0.1'
+    
+    depends_on 'cmake' => :build
+    
+    
+    def install
+        cmake_args = std_cmake_parameters.split
+        
+        cmake_args << "-DUSE_AYSNC_COMMS:BOOL=ON"
+        
+        cmake_args << "-DENABLE_V10_COMPATIBILITY:BOOL=ON"
+        
+        system "cmake", ".", *cmake_args
+        system "make"
+        system "make install"
+        
+    end
+    
+    def test
+        system "#{bin}/MOOSDB --help"
+    end
+    
+    
 end
