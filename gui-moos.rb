2611a86af7b848dc62cbd414f3b2ae9d1e0246b9
Paul Newman <pnewman@robots.ox.ac.uk>
2012-12-12 12:25:57 +0000

    added ui-moos rename

--- gui-moos.rb
+++ gui-moos.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
-class GuiMoos < Formula
+class UiMoos < Formula
     homepage 'www.robots.ox.ac.uk/~pnewman/TheMOOS'
-    url 'https://github.com/themoos/gui-moos/archive/devel.zip'
-    head 'https://github.com/themoos/gui-moos.git', :branch => 'devel'
+    url 'https://github.com/themoos/ui-moos/archive/devel.zip'
+    head 'https://github.com/themoos/ui-moos.git', :branch => 'devel'
     
     #sha1 'a077343435f6b0f2cfa22d23066dcb8c7571d852'
     version '10.0.1'
